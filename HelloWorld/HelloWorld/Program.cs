79c72d8eb77bd47a5734c0a220f1983b6a4379ef
akexinad <danixeka@gmail.com>
2019-08-15 20:12:35 +1000

    fundamentals to csharp and .net

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Remember that Console is a class. Like any class it has methods.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
