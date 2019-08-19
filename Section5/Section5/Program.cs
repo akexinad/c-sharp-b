11c4c3b81b1a3d076968525b8e468e1842d2ce17
danny <danny.ceccattini@lendlease.com>
2019-08-19 14:57:20 +1000

    exercises

---
+++ Section5/Section5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Section5
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            // 43. EXERCISES
+            // =============
+
+
+            // EXERCISE 1
+
+            var number = 11;
+
+            if (number == 10)
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+
+
+            // EXERCISE 2
+
+            var num1 = 5;
+            var num2 = 4;
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+            }
+
+
+            // EXERCISE 3
+
+            var dim1 = 4;
+            var dim2 = 5;
+
+            if (dim1 > dim2)
+            {
+                Console.WriteLine("portrait");
+            }
+            else
+            {
+                Console.WriteLine("landscape");
+            }
+
+
+            // EXERCISE 4
+
+            Console.Write("Your Speed: ");
+            var speed = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The Speed Limit: ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            if (speed < speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                var kmPerDemeritPoint = 5;
+                var demeritPoints = (speed - speedLimit) / kmPerDemeritPoint;
+                if (demeritPoints >= 12)
+                {
+                    Console.WriteLine("SUSPENDED");
+                }
+                else
+                {
+                    Console.WriteLine($"You have lost { demeritPoints } demerit points.");
+                }
+            }
+        }
+    }
+}
